895b358b865e81344b09dad65db8736d98220f8a
dkuralka <dkuralka@in.ibm.com>
2017-03-24 11:13:48 +0530

    login page intergration with Marble

--- routes/site_router.js
+++ routes/site_router.js
@@ -25,13 +25,33 @@ function build_bag(){
 // ============================================================================================================================
 // Home
 // ============================================================================================================================
-router.route('/').get(function(req, res){
+/*router.route('/').get(function(req, res){
 	res.redirect('/p1');
-});
+});*/
 
+router.route('/').get(function(req, res){
+	res.redirect('/login');
+});
 // ============================================================================================================================
 // Part 1
 // ============================================================================================================================
+router.route('/login').get(function(req, res){
+	res.render('login', {title: 'login', bag: build_bag()});
+});
+
+app.post('/', function(req, res){
+	console.log(req.body['user']);
+	if(req.body['user'] == "admin" && req.body['pass'] == "admin"){
+		res.redirect('/p1');
+	}
+	else{
+		res.render('login', {
+			title : 'User id password incorrect'
+			//udata : req.session.user
+		});
+	}
+
+
 router.route('/p1').get(function(req, res){
 	res.render('part1', {title: 'Marbles Part 1', bag: build_bag()});
 });
